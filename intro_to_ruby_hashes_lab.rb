487eec778910f06faa6678bda8e4cdd8e4f81e23
Dawn Cronin <dawncronin7@gmail.com>
2019-10-13 23:02:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  monopoly =	{:money => 100, :railroads => 0}
+  monopoly =	{:money => 100, :railroads => {}}
 end
 
 def monopoly_with_third_tier
